23ffa4ff05944f24b43662008bc78a2b928d7ef4
monika-stempniak <mstempniak@gmail.com>
2019-02-18 14:31:02 +0100

    Create Menu

---
+++ src/App/components/Menu/Menu.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavLink, } from 'react-router-dom';
+
+import styles from "./Menu.module.scss"
+
+const Menu = () => (
+  <nav className={styles.navbar}>
+    <div className={styles.container}>
+      <ul className={styles.nav}>
+        <li>
+          <NavLink exact to="/" activeClassName={styles.activeLink}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/catalog" activeClassName={styles.activeLink}>Catalog</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" activeClassName={styles.activeLink}>About</NavLink>
+        </li>
+      </ul>
+    </div>
+</nav>
+);
+
+export default Menu;
