bebc2c7a5eb1b971cc986e9c9dee1b325b5651d4
monika-stempniak <mstempniak@gmail.com>
2019-06-02 21:01:15 +0200

    Setup redux store

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
 
 import "./scss/main.scss";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { store } from "./store";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
