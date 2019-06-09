09144206ef517f1d598866409a196ab074704a97
monika-stempniak <mstempniak@gmail.com>
2019-06-09 19:45:33 +0200

    Handle route bug

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
 
 import Menu from "./components/Menu/Menu";
 import Footer from "./components/Footer/Footer";
-import HomePage from "../pages/HomePage/HomePage";
-import CatalogPage from "../pages/CatalogPage/CatalogPage";
-import AboutPage from "../pages/AboutPage/AboutPage";
-import CartPage from "../pages/CartPage/CartPage";
 
 import styles from "./App.module.scss";
+
+const AboutPage = withRouter(React.lazy(() => import("../pages/AboutPage/AboutPage")));
+const CatalogPage = withRouter(React.lazy(() => import("../pages/CatalogPage/CatalogPage")));
+const CartPage = withRouter(React.lazy(() => import("../pages/CartPage/CartPage")));
+const HomePage = withRouter(React.lazy(() => import("../pages/HomePage/HomePage")));
 
 class App extends Component {
   render() {
@@ -17,10 +18,12 @@ class App extends Component {
         <div className={styles.wrapper}>
           <Menu />
           <Switch>
-            <Route exact path="/" render={() => <HomePage/>}/>
-            <Route path="/catalog" render={() => <CatalogPage />}/>
-            <Route path="/about" component={AboutPage}/>  
-            <Route path="/cart" component={CartPage}/>  
+              <Suspense fallback={<div>Loading...</div>}>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/catalog" component={CatalogPage}/>
+                <Route path="/cart" component={CartPage}/>  
+                <Route path="/about" component={AboutPage}/>  
+              </Suspense>
             <Route render={() => <h1>Page not found</h1>}/>
           </Switch>
           <Footer />
